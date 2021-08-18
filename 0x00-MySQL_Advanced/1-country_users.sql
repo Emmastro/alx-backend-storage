62f48f43f76394c2565006fa69ff3c92834ebf1c
Emmanuel Murairi <emurairi18@alastudents.org>
2021-08-18 18:52:27 +0400

    submit

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create a table users with country attribute
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
---list all bands with Glam rock as their main style, ranked by longevity
+-- Lists all bands with Glam as their main style, ranked by their longevity
 
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan
 FROM metal_bands
