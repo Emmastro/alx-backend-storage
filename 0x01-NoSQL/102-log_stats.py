27e63c3c523dfce139507c75e9a140d629b523ab
Emmanuel Murairi <emurairi18@alastudents.org>
2021-08-21 19:04:11 +0400

    submit

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -21,6 +21,28 @@ def nginx_stats_check():
     status = collec_nginx.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status))
 
+    print("IPs:")
+
+    top_IPs = collec_nginx.aggregate([
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for top_ip in top_IPs:
+        count = top_ip.get("count")
+        ip_address = top_ip.get("ip")
+        print("\t{}: {}".format(ip_address, count))
+
 
 if __name__ == "__main__":
     nginx_stats_check()
